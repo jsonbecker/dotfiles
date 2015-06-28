b1f2044e707b6671c462df9a6831c508f46e9eb9
jbecker <jason.paul.becker@gmail.com>
2015-06-28 13:32:24 -0400

    Refactor to use bash functions for brew
    Using the examples from `cowboy/dotifles/init`, I have included new functions for:
    
    * brew_tap_kegs
    * brew_install_recipes
    * brew_install_casks
    * brew_install_fonts

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -5,28 +5,74 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
 brew update
 
+## From cowboy/dotfiles/init
+function brew_tap_kegs() {
+  kegs=($(setdiff "${kegs[*]}" "$(brew tap)"))
+  if (( ${#kegs[@]} > 0 )); then
+    e_header "Tapping Homebrew kegs: ${kegs[*]}"
+    for keg in "${kegs[@]}"; do
+      brew tap $keg
+    done
+  fi
+}
+
+function brew_install_recipes() {
+  recipes=($(setdiff "${recipes[*]}" "$(brew list)"))
+  if (( ${#recipes[@]} > 0 )); then
+    e_header "Installing Homebrew recipes: ${recipes[*]}"
+    for recipe in "${recipes[@]}"; do
+      brew install $recipe
+    done
+  fi
+}
+
+function brew_install_casks(){
+  casks=($(setdiff "${casks[*]}" "$(brew cask list 2>/dev/null)"))
+  if (( ${#casks[@]} > 0 )); then
+    e_header "Installing Homebrew casks: ${casks[*]}"
+    for cask in "${casks[@]}"; do
+      brew cask install $cask
+    done
+    brew cask cleanup
+  fi
+}
+
+function brew_install_fonts(){
+  fonts=($(setdiff "${fonts[*]}" "$(brew cask list 2>/dev/null)"))
+  if (( ${#fonts[@]} > 0 )); then
+    e_header "Installing Homebrew fonts: ${fonts[*]}"
+    for font in "${fonts[@]}"; do
+      brew cask install $font
+    done
+    brew cask cleanup
+  fi
+}
+
+
 ## Install Stuff with Brew
-brew install ffmpeg
-brew install mkvtoolnix
-brew install mp4v2
-brew install mplayer
-brew install zsh 
-brew install zsh-completions
-brew install pandoc
-brew install git
-brew install libyaml
-brew install libmatroska
-brew install vim
-brew install macvim
-brew install s3cmd
-brew install wget
-brew install openssl
-brew install curl
-brew install flac
-brew install pandoc
-brew install postgresql
-brew install sqlite
-brew install handbrakecli
+recipes =(
+    ffmpeg
+    mkvtoolnix
+    mp4v2
+    mplayer
+    zsh
+    zsh-completions
+    pandoc
+    git
+    libyaml
+    libmatroska
+    vim
+    macvim
+    s3cmd
+    wget
+    curl
+    flac
+    pandoc
+    postgresql
+    sqlite
+)
+brew_install_recipes
+
 ## Define Git stuff
 git config --global user.name "jsonbecker"
 git config --global user.email "jason@jbecker.co"
@@ -36,31 +82,64 @@ git config --global credential.helper osxkeychain
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 ## Install applications with brew-cask
-brew tap caskroom/cask
+kegs =(
+    caskroom/cask
+    caskroom/fonts
+    caskroom/versions
+)
+
+brew_tap_kegs
+
 brew install brew-cask
-brew tap caskroom/versions
 brew update && brew upgrade brew-cask && brew cleanup
-brew cask install r
-brew cask install r-studio
-brew cask install vlc
-brew cask install postbox
-brew cask install iterm2
-brew cask install kaleidoscope
-brew cask install sublime-text3
-brew cask install bartender
-brew cask install synology-cloud-station
-brew cask install carbon-copy-cloner
-brew cask install screenhero
-brew cask install transmit
-brew cask install dropbox
-brew cask install betterzipql
-brew cask install qlcolorcode
-brew cask install qlmarkdown
-brew cask install qlprettypatch
-brew cask install qlstephen
-brew cask install quicklook-csv
-brew cask install quicklook-json
-brew cask install quicknfoz Install
+
+casks=(
+    handbrakecli
+    r
+    r-studio
+    vlc
+    postbox
+    iterm2
+    macvim
+    kaleidoscope
+    sublime-text3
+    bartender
+    synology-cloud-station
+    carbon-copy-cloner
+    screenhero
+    transmit
+    dropbox
+    betterzipql
+    qlcolorcode
+    qlmarkdown
+    qlprettypatch
+    qlstephen
+    quicklook-csv
+    quicklook-json
+    backblaze
+    balsamiq-mockups
+    tower
+    cleanmymac
+    colorpicker-skalacolor
+    transmit
+    slack
+    todoist
+)
+
+brew_install_casks
+
+## Install Fonts
+
+fonts =(
+    font-open-sans
+    font-montserrat
+    font-source-code-pro
+    font-source-code-pro-for-powerline
+    font-source-sans-pro
+    font-fauna-one
+)
+
+brew_install_fonts
 
 ## Don Melton video_transcodiing
 
