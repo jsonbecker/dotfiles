f1df7e404e4340b334648f6a542baa82b818af47
jbecker <jason.paul.becker@gmail.com>
2015-06-28 21:26:12 -0400

    Restructured directory for easier symlinking and added link.sh
    link.sh is taken from tomsquest/dotfiles and has a handy function that
    will generate symlinks if one does not already exist for a series of
    important dotfiles. As such, I have pulled up vimrc and zshrc so that
    they are not contained within zsh/ and vim/ which now can be directly
    symlinked to ~/.zsh and ~/.vim.
    
    I will add additional files (some are commented out that came from the
    original) as they are ready.
    
    Also, I have removed fish/ since I have decided to stick with zsh and
    oh-my-zsh and don't need those customizations anymore.

---
+++ link.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+# Taken from tomsquest/dotfiles
+set +e
+
+function link_if_missing() {
+  SRC=$1
+  DEST=$2
+
+  if ! [ -L $DEST ]; then
+    ln -ivs $SRC $DEST
+  else
+    echo "Skipping, link already exists: $SRC"
+  fi
+}
+
+# link_if_missing $PWD/bin        $HOME/bin
+link_if_missing $PWD/vim        $HOME/.vim
+# link_if_missing $PWD/zsh        $HOME/.zsh
+# link_if_missing $PWD/ackrc      $HOME/.ackrc
+# link_if_missing $PWD/gitconfig  $HOME/.gitconfig
+# link_if_missing $PWD/gitignore  $HOME/.gitignore
+# link_if_missing $PWD/imwheelrc  $HOME/.imwheelrc
+# link_if_missing $PWD/irbrc      $HOME/.irbrc
+# link_if_missing $PWD/plsqlrc    $HOME/.plsqlrc
+# link_if_missing $PWD/rvmrc      $HOME/.rvmrc
+# link_if_missing $PWD/tmux.conf  $HOME/.tmux.conf
+link_if_missing $PWD/gvimrc     $HOME/.gvimrc
+link_if_missing $PWD/vimrc      $HOME/.vimrc
+link_if_missing $PWD/zshrc      $HOME/.zshrc
+link_if_missing $PWD/zsh/jbecker.zsh-theme $HOME/.oh-my-zsh/themes/jbecker.zsh-theme
