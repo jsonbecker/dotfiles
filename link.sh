62d88f56611d7dd41255e7500a97544d203b28bc
jbecker <jason.paul.becker@gmail.com>
2015-07-05 22:47:05 -0400

    Added additional files for symlinking

--- link.sh
+++ link.sh
@@ -13,18 +13,19 @@ function link_if_missing() {
   fi
 }
 
-# link_if_missing $PWD/bin        $HOME/bin
-link_if_missing $PWD/vim        $HOME/.vim
-# link_if_missing $PWD/zsh        $HOME/.zsh
-# link_if_missing $PWD/ackrc      $HOME/.ackrc
-# link_if_missing $PWD/gitconfig  $HOME/.gitconfig
-# link_if_missing $PWD/gitignore  $HOME/.gitignore
-# link_if_missing $PWD/imwheelrc  $HOME/.imwheelrc
-# link_if_missing $PWD/irbrc      $HOME/.irbrc
-# link_if_missing $PWD/plsqlrc    $HOME/.plsqlrc
-# link_if_missing $PWD/rvmrc      $HOME/.rvmrc
-# link_if_missing $PWD/tmux.conf  $HOME/.tmux.conf
-link_if_missing $PWD/gvimrc     $HOME/.gvimrc
-link_if_missing $PWD/vimrc      $HOME/.vimrc
-link_if_missing $PWD/zshrc      $HOME/.zshrc
+# link_if_missing $PWD/bin          $HOME/bin
+link_if_missing $PWD/bash_profile   $HOME/.bash_profile
+link_if_missing $PWD/vim            $HOME/.vim
+link_if_missing $PWD/zsh            $HOME/.zsh
+# link_if_missing $PWD/ackrc        $HOME/.ackrc
+link_if_missing $PWD/gitconfig      $HOME/.gitconfig
+# link_if_missing $PWD/gitignore    $HOME/.gitignore
+# link_if_missing $PWD/imwheelrc    $HOME/.imwheelrc
+# link_if_missing $PWD/irbrc        $HOME/.irbrc
+# link_if_missing $PWD/plsqlrc      $HOME/.plsqlrc
+# link_if_missing $PWD/rvmrc        $HOME/.rvmrc
+# link_if_missing $PWD/tmux.conf    $HOME/.tmux.conf
+link_if_missing $PWD/gvimrc         $HOME/.gvimrc
+link_if_missing $PWD/vimrc          $HOME/.vimrc
+link_if_missing $PWD/zshrc          $HOME/.zshrc
 link_if_missing $PWD/zsh/jbecker.zsh-theme $HOME/.oh-my-zsh/themes/jbecker.zsh-theme
