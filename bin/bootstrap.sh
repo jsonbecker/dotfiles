a5ce87c7d6e200d724970d819c76592430816868
jsonbecker <jason@jbecker.co>
2021-01-27 16:25:31 -0500

    Minor updates

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -11,107 +11,7 @@ brew update
 
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
-## From cowboy/dotfiles/init
-
-function e_header()   { echo -e "\n\033[1m$@\033[0m"; }
-function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
-function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
-function e_arrow()    { echo -e " \033[1;34m➜\033[0m  $@"; }
-
-function setdiff() {
-  local debug skip a b
-  if [[ "$1" == 1 ]]; then debug=1; shift; fi
-  if [[ "$1" ]]; then
-    local setdiffA setdiffB setdiffC
-    setdiffA=($1); setdiffB=($2)
-  fi
-  setdiffC=()
-  for a in "${setdiffA[@]}"; do
-    skip=
-    for b in "${setdiffB[@]}"; do
-      [[ "$a" == "$b" ]] && skip=1 && break
-    done
-    [[ "$skip" ]] || setdiffC=("${setdiffC[@]}" "$a")
-  done
-  [[ "$debug" ]] && for a in setdiffA setdiffB setdiffC; do
-    echo "$a ($(eval echo "\${#$a[*]}")) $(eval echo "\${$a[*]}")" 1>&2
-  done
-  [[ "$1" ]] && echo "${setdiffC[@]}"
-}
-
-function brew_tap_kegs() {
-  kegs=($(setdiff "${kegs[*]}" "$(brew tap)"))
-  if (( ${#kegs[@]} > 0 )); then
-    e_header "Tapping Homebrew kegs: ${kegs[*]}"
-    for keg in "${kegs[@]}"; do
-      brew tap $keg
-    done
-  fi
-}
-
-function brew_install_recipes() {
-  recipes=($(setdiff "${recipes[*]}" "$(brew list)"))
-  if (( ${#recipes[@]} > 0 )); then
-    e_header "Installing Homebrew recipes: ${recipes[*]}"
-    for recipe in "${recipes[@]}"; do
-      brew -v install $recipe
-    done
-  fi
-}
-
-function brew_install_casks(){
-  casks=($(setdiff "${casks[*]}" "$(brew cask list 2>/dev/null)"))
-  if (( ${#casks[@]} > 0 )); then
-    e_header "Installing Homebrew casks: ${casks[*]}"
-    for cask in "${casks[@]}"; do
-      brew cask install $cask
-    done
-    brew cask cleanup
-  fi
-}
-
-function brew_install_fonts(){
-  fonts=($(setdiff "${fonts[*]}" "$(brew cask list 2>/dev/null)"))
-  if (( ${#fonts[@]} > 0 )); then
-    e_header "Installing Homebrew fonts: ${fonts[*]}"
-    for font in "${fonts[@]}"; do
-      brew cask install $font
-    done
-    brew cask cleanup
-  fi
-}
-
-
-## For elasticsearch
-brew install Caskroom/cask/java
-
-## Install Stuff with Brew
-recipes=(
-    nodejs
-    elasticsearch
-    ffmpeg
-    mkvtoolnix
-    mp4v2
-    mplayer
-    zsh
-    zsh-completions
-    pandoc
-    git
-    libyaml
-    libmatroska
-    vim
-    macvim
-    s3cmd
-    wget
-    curl
-    flac
-    pandoc
-    postgresql
-    sqlite
-    trash
-)
-brew_install_recipes
-
+brew bundle install --file ../Brewfile
 ## Define Git stuff
 git config --global user.name "jsonbecker"
 git config --global user.email "jason@jbecker.co"
@@ -120,80 +20,11 @@ git config --global credential.helper osxkeychain
 ## Install oh-my-zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
-## Install applications with brew-cask
-kegs=(
-    caskroom/cask
-    caskroom/fonts
-    caskroom/versions
-)
-
-brew_tap_kegs
-
-brew install caskroom/cask/brew-cask
-brew update && brew upgrade brew-cask && brew cleanup
-
-casks=(
-    handbrakecli
-    r
-    rstudio
-    vlc
-    postbox
-    iterm2
-    macvim
-    kaleidoscope
-    sublime-text3
-    bartender
-    synology-cloud-station
-    carbon-copy-cloner
-    screenhero
-    transmit
-    dropbox
-    betterzipql
-    qlcolorcode
-    qlmarkdown
-    qlprettypatch
-    qlstephen
-    quicklook-csv
-    quicklook-json
-    backblaze
-    balsamiq-mockups
-    tower
-    cleanmymac
-    colorpicker-skalacolor
-    transmit
-    slack
-    todoist
-    dbeaver-community
-)
-
-brew_install_casks
-
-## Install Fonts
-
-fonts=(
-    font-lato
-    font-hack
-    font-open-sans
-    font-montserrat
-    font-source-code-pro
-    font-source-code-pro-for-powerline
-    font-source-sans-pro
-    font-fauna-one
-)
-
-brew_install_fonts
-
 ## Don Melton video_transcodiing
 
 gem install video_transcoding
+gem install other_transcoding
 
-## Mac OSX settings taken from brandonb927/osx-for-hackers.sh gist
-echo ""
-echo "Expanding the save panel by default"
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
- 
 echo ""
 echo "Automatically quit printer app once the print jobs complete"
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
